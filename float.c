47ce124c09c334905f1878329bd5dc9acdff4003
BrianT <304@DESKTOP-OBFHSVF>
2024-06-01 11:26:35 -0700

    first commit

---
+++ float.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+int main() {
+
+    float fValue;
+
+    printf("Enter a float value \n");
+    scanf("%f", &fValue);
+
+    printf("fValue = %f \n", fValue); 
+
+    return 0;
+}
